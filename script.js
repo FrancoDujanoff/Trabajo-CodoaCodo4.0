c88dd6b1e28d1d94b804e88ada6b870489671ab9
FrancoDujanoff <fdujanoff@gmail.com>
2023-06-03 11:48:48 -0300

    Corregido y agregado de mas funcionalidad

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const cantidad= document.getElementById("cantidad");
+const descuento= document.getElementById("categoria");
+const nombre = document.getElementById("nombre");
+const apellido = document.getElementById("apellido");
+const correo = document.getElementById("email");
+
+function estudiante(){
+    const descuento= document.getElementById("categoria");
+    descuento.value = "estudiante";
+}
+function trainee(){
+    const descuento= document.getElementById("categoria");
+    descuento.value = "trainee";
+}
+function junior(){
+    const descuento= document.getElementById("categoria");
+    descuento.value = "junior";
+}
+
 function norecargar(){
     let form = document.querySelector("form");
     if (form.checkValidity()){
@@ -12,11 +31,13 @@ function norecargar2(){
 }
 
 function borrar(){
-    const cantidad=document.getElementById("cantidad");
-    const descuento=document.getElementById("categoria");
+    const cantidad= document.getElementById("cantidad");
+    const descuento= document.getElementById("categoria");
     const nombre = document.getElementById("nombre");
     const apellido = document.getElementById("apellido");
     const correo = document.getElementById("email");
+    const boton = document.getElementById("botonEnviar");
+    const div = document.getElementById("no");
 
     cantidad.value = 0;
     descuento.value = 0;
@@ -24,14 +45,19 @@ function borrar(){
     apellido.value = "";
     correo.value = "";
 
-    document.getElementById("total_a_pagar").innerHTML = "Total a pagar:$";
+    boton.disabled = false;
+    nombre.disabled = false;
+    apellido.disabled = false;
+    correo.disabled = false;
+    cantidad.disabled = false;
+    div.innerHTML = "";
 
+    document.getElementById("total_a_pagar").innerHTML = "Total a pagar:$";
 }
 
-
 function resumen(){
-    const cantidad=document.getElementById("cantidad");
-    const descuento=document.getElementById("categoria");
+    const cantidad= document.getElementById("cantidad");
+    const descuento= document.getElementById("categoria");
 
     let c = cantidad.value;
     let d = descuento.value;
@@ -51,6 +77,31 @@ function resumen(){
     let totalfinal = total - (total*d1);
 
     document.getElementById("total_a_pagar").innerHTML = "Total a pagar:$" + totalfinal;
+}
 
+function botonEnvio(){
+    const clase = document.getElementById("categoria").value;
+    const boton = document.getElementById("botonEnviar");
+    const div = document.getElementById("no");
+    const cantidad= document.getElementById("cantidad");
+    const nombre = document.getElementById("nombre");
+    const apellido = document.getElementById("apellido");
+    const correo = document.getElementById("email");
+
+    if(clase === "nada"){
+        boton.disabled = true;
+        nombre.disabled = true;
+        apellido.disabled = true;
+        correo.disabled = true;
+        cantidad.disabled = true;
+        div.innerHTML = "<p>*La compra esta deshabilitada para personas sin descuento*</p>"
+    } else {
+        boton.disabled = false;
+        nombre.disabled = false;
+        apellido.disabled = false;
+        correo.disabled = false;
+        cantidad.disabled = false;
+        div.innerHTML = "";
+    }
 
 }
